4dfae9cef4e7206e2b9331586527bf58cc8525eb
syab-syab <syabsyabsyabsyab@gmail.com>
2023-10-21 20:53:12 +0900

    タイトル変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ export const Header = () => {
   return (
     <>
       <header className="header">
-        <p className="sub-title">音楽理論の学習や耳コピなどに役立ててください。</p>
-        <h1 className="title">ダイアトニックコードの</h1>
-        <h1 className="title"><span className="emphasis">カンペ</span></h1>
+        {/* <p className="sub-title">音楽理論の学習や耳コピなどに役立ててください。</p> */}
+        <p className="sub-title">ダイアトニックコードのカンペ</p>
+        <h1 className="title">Diatonic <span className="emphasis">Cheat</span></h1>
       </header>
     </>
   )
